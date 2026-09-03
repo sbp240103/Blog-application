9a22565d005ae6dedc93fa454a0b2a137a15e940
sbp240103 <154756072+sbp240103@users.noreply.github.com>
2026-09-04 00:19:25 +0530

    added AppConstants for service name and pagination defaults

---
+++ src/main/java/com/project/app/utils/AppConstants.java
@@ -0,0 +1,14 @@
+package com.project.app.utils;
+
+public final class AppConstants {
+
+	public static final String SERVICE_NAME = "blog-application";
+
+	public static final int DEFAULT_PAGE_NUMBER = 0;
+
+	public static final int DEFAULT_PAGE_SIZE = 10;
+
+	private AppConstants() {
+	}
+
+}
