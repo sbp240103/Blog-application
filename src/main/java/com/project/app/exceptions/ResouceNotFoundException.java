e7042f317f4c9a61c5452d6f398a5ee24fe7c123
sbp240103 <154756072+sbp240103@users.noreply.github.com>
2026-09-06 16:48:19 +0530

    added global exception handler for resource not found error

---
+++ src/main/java/com/project/app/exceptions/ResouceNotFoundException.java
@@ -0,0 +1,19 @@
+package com.project.app.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter 
+
+public class ResouceNotFoundException extends RuntimeException {
+    String resourceName;
+    String fieldName;
+    long fieldValue;
+    public ResouceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+        super(String.format("%s not found with % : %l", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
---
+++ src/main/java/com/project/app/services/impl/UserServiceImpl.java
@@ -0,0 +1,85 @@
+package com.project.app.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties.Apiversion.Use;
+import org.springframework.stereotype.Service;
+
+import com.project.app.payloads.UserDto;
+import com.project.app.repositories.UserRepo;
+import com.project.app.services.UserService;
+import com.project.app.entities.*;
+import com.project.app.exceptions.ResouceNotFoundException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired 
+    private UserRepo userRepo;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        
+        User user = this.dtoToUser(userDto);
+        User savedUser = this.userRepo.save(user);
+
+        return this.userToDto(savedUser);
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, Integer userId) {
+        User user = this.userRepo.findById(userId)
+                .orElseThrow(() -> new ResouceNotFoundException("User", "Id", userId));
+
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setAbout(userDto.getAbout());
+        user.setPassword(userDto.getPassword());
+
+        User updatedUser= this.userRepo.save(user);
+        UserDto userDto1= this.userToDto(updatedUser);
+        return userDto1;
+    }
+
+    @Override
+    public UserDto getUserById(Integer userId) {
+        User user = this.userRepo.findById(userId).orElseThrow(()-> new ResouceNotFoundException("User", "Id", userId));
+        return this.userToDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = this.userRepo.findAll();
+
+        users.stream().map(user)
+    }
+
+    @Override
+    public void deleteUser(Integer userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    public  User dtoToUser(UserDto userDto){
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setAbout(userDto.getAbout());
+        return user;
+    }
+
+    public  UserDto userToDto(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPassword(user.getPassword());
+        userDto.setAbout(user.getAbout());
+        return userDto;
+    }
+
+    
+}
