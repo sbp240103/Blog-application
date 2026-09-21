0678134fd03cedffa8bae57ff2081e8600cd7cba
sbp240103 <154756072+sbp240103@users.noreply.github.com>
2026-09-22 01:26:51 +0530

    added missing annotations for commentdto

--- src/main/java/com/project/app/payloads/CommentDto.java
+++ src/main/java/com/project/app/payloads/CommentDto.java
@@ -1,5 +1,12 @@
 package com.project.app.payloads;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor 
+@Getter 
+@Setter 
 public class CommentDto {
 
     private int id;
